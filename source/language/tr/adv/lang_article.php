bdfd71e032057f39e6d9ac86fa7768176a7eee10
ulascemh <76663782+ulascemh@users.noreply.github.com>
2023-03-31 17:31:38 +0300

    adv/lang_article.php TRANSLATED

--- source/language/tr/adv/lang_article.php
+++ source/language/tr/adv/lang_article.php
@@ -13,14 +13,14 @@ if(!defined('IN_DISCUZ')) {
 
 $lang = array
 (
-	'article_name'			=> 'Portal Article Ads',//'门户 文章广告',
-	'article_desc'			=> 'Display mode: Article advertising displayed on the article page, Into the content area surrounded by ads and related reading above two kinds of ads.',//'展现方式: 文章广告显示于文章页面，分为内容区域环绕广告和相关阅读上方广告 2 种。',
-	'article_position'		=> 'Ads Position',//'投放位置',
-	'article_position_comment'	=> 'It is divided into 3 positions, Where relevant ads above the reading of two adjacent display position',//'分为 3 个位置，其中相关阅读上方广告有 2 个紧邻的显示位置',
-	'article_position_float'	=> 'Floating ads (content area flow around the Ads)',//'内容区域环绕',
-	'article_position_up'		=> 'At the top of content (I)',//'相关阅读上方(上)',
-	'article_position_down'		=> 'Below the content (II)',//'相关阅读上方(下)',
-	'article_category'		=> 'Show in the Category',//'投放频道分类',
-	'article_category_comment'	=> 'Set the category to display Ads in',//'设置广告投放的频道分类',
+	'article_name'			=> 'Portal Makale Reklamı',//'门户 文章广告',
+	'article_desc'			=> 'Görüntüleme Modu: Makale sayfasında görüntülenen makale reklamları, reklamlarla çevrili içerik alanına ve iki tür reklamın üzerindeki ilgili okumaya.',//'展现方式: 文章广告显示于文章页面，分为内容区域环绕广告和相关阅读上方广告 2 种。',
+	'article_position'		=> 'Reklam Konumu',//'投放位置',
+	'article_position_comment'	=> 'İlgili okumanın üzerinde 2 bitişik ekran konumu ile 3 konuma ayrılmıştır',//'分为 3 个位置，其中相关阅读上方广告有 2 个紧邻的显示位置',
+	'article_position_float'	=> 'Yüzen reklamlar (Reklamların etrafında içerik alanı akışı)',//'内容区域环绕',
+	'article_position_up'		=> 'İçeriğin en üstünde (I)',//'相关阅读上方(上)',
+	'article_position_down'		=> 'İçeriğin altında (II)',//'相关阅读上方(下)',
+	'article_category'		=> 'Kategoride Göster',//'投放频道分类',
+	'article_category_comment'	=> 'Reklamların görüntüleneceği kategoriyi ayarlama',//'设置广告投放的频道分类',
 );
 
